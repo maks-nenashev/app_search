6aff64c6599ac8538d53fd3108e3fea92edf32a1
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2024-02-27 23:13:27 +0100

    added new model

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -5,5 +5,14 @@ class Thing < ApplicationRecord
     has_many :comments, as: :commentable, dependent: :destroy # Podkluchenie "Comments" Rukami 
  
     belongs_to :local
+    
 
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["image", "created_at", "id", "local_id", "text", "title", "updated_at"]
+        end
+      def self.ransackable_associations(auth_object = nil)
+        ["comments", "local"]
+        end
+      
 end
